b86515ae965b8ac7b69ad6ce8dd3b87e4469f40e
Martin Liu <Martin Liu>
2021-12-16 14:43:16 +0800

    add docker scripts

---
+++ docker/docker_into.sh
@@ -0,0 +1,1 @@
+docker exec -it gt_mm3d bash
---
+++ docker/docker_start.sh
@@ -0,0 +1,2 @@
+docker run --gpus all -it -v /home/revolution/guangtong/mmdetection3d:/mmdetection3d \
+-v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v /Dataset:/mmdetection3d/data --name gt_mm3d mmdetection3d:gt_mm3d_latest
