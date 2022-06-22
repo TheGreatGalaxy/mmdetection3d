3da13a4db99fed15e5bd7c065f1c8d9e7b95268e
Guangtong Liu <liuguangtong@revolution.auto>
2022-06-22 11:27:30 +0800

    improve docker file

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,3 +1,8 @@
+# This docker file path
+DOCKER_FILE_PATH="docker/Dockerfile"
+# Your code path, will project into docker path: /project.
+CODE_PATH="/home/revolution/guangtong/mmdetection3d"
+
 IMAGE="mmdetection_v3"
 TAG="latest"
 CONTAINER="gt_mm3d_v4"
@@ -5,7 +10,7 @@ CONTAINER="gt_mm3d_v4"
 function create() {
   local img_ver=$IMAGE:$TAG
   echo "Will create container: $CONTAINER, from image: $img_ver"
-  xhost +  && docker run --gpus all -it -v /home/revolution/guangtong/mmdetection3d:/mmdetection3d -v /tmp/.X11-unix:/tmp/.X11-unix \
+  xhost +  && docker run --gpus all -it -v ${CODE_PATH}:/mmdetection3d -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /Dataset:/mmdetection3d/data -e DISPLAY=$DISPLAY --name $CONTAINER $img_ver
 }
 
@@ -13,22 +18,30 @@ function start() {
   docker start $CONTAINER
 }
 
-
 function into() {
   echo "Will into container: $CONTAINER"
   docker exec -it $CONTAINER bash
 }
 
+function build() {
+  echo "Will build docker image: $IMAGE, with tag: $TAG"
+  docker build -f $DOCKER_FILE_PATH -t $IMAGE:$TAG .
+}
+
 function help() {
-  echo "bash docker/docker.sh [option]
-        option: 
+  echo "Run command:
+          bash docker/docker.sh [Option]
+        Option:
+          b, build: build docker image 
           c, create: create a container from the given image 
           s, start: start the given container
           i, into: into the given container"
 }
 
 function main() {
-  if [[ $1 == "create" || $1 == "c" ]]; then
+  if [[ $1 == "build" || $1 == "b" ]]; then
+    build
+  elif [[ $1 == "create" || $1 == "c" ]]; then
     create
   elif [[ $1 == "start" || $1 == "s" ]]; then 
     start
